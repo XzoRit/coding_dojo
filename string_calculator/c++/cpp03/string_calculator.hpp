74df030cea845828eb0847811ed6952fb74dd700
XzoRit <xzorit@hotmail.com>
2013-10-22 18:52:29 +0200

    simplyfied test cases and add method of cpp03 implementation

--- string_calculator/c++/cpp03/string_calculator.hpp
+++ string_calculator/c++/cpp03/string_calculator.hpp
@@ -15,56 +15,48 @@
 
 namespace StringCalculator
 {
-  template<typename IterOut>
-  void make_ints_from_str(std::string const& numsAsString, std::string const& sep, IterOut out)
-  {
-    std::vector<std::string> sepNumsAsString;
-    StrUtil::split(numsAsString, sep, std::back_inserter(sepNumsAsString));
-
-    std::vector<int> ints(sepNumsAsString.size(), 0);
-    std::transform(sepNumsAsString.begin(), sepNumsAsString.end(),
-		   ints.begin(), &Convert::String::to<int>);
-
-    if(ints.end() == std::find_if(ints.begin(), ints.end(), std::bind2nd(std::less<int>(), 0)))
-      {
-	std::copy(ints.begin(), ints.end(), out);
-      }
-    else
-      {
-        std::vector<int> negNums;
-        std::remove_copy_if(ints.begin(), ints.end(),
-			    std::back_inserter(negNums), std::bind2nd(std::greater_equal<int>(), 0));
-
-	std::ostringstream exceptTxt;
-	exceptTxt << "Negative numbers not allowed: ";
-	std::copy(negNums.begin(), negNums.end(),
-		  std::ostream_iterator<int>(exceptTxt, " "));
-
-	throw std::invalid_argument(exceptTxt.str());
-      }
-  }
-
-  std::pair<std::string, std::string> extractSep(std::string const& parse)
-  {
+std::pair<std::string, std::string> extractSep(std::string const& parse)
+{
     static std::string::size_type const SizeOfSepSection = 4;
 
     if(parse.size() >= SizeOfSepSection && parse[0] == '/')
-      return std::make_pair(std::string(parse.begin() + SizeOfSepSection, parse.end()), std::string(1, parse[2]));
+        return std::make_pair(std::string(parse.begin() + SizeOfSepSection, parse.end()), std::string(1, parse[2]));
     else
-      return std::make_pair(parse, ",\n");
-  }
+        return std::make_pair(parse, ",\n");
+}
 
-  int add(std::string const& nums)
-  {
+int add(std::string const& nums)
+{
     if(nums.empty()) return 0;
 
     std::pair<std::string, std::string> const numsAndSep = extractSep(nums);
 
-    std::vector<int> ints;
-    make_ints_from_str(numsAndSep.first, numsAndSep.second, std::back_inserter(ints));
+    std::vector<std::string> sepNumsAsString;
+    StrUtil::split(numsAndSep.first, numsAndSep.second, std::back_inserter(sepNumsAsString));
 
-    return std::accumulate(ints.begin(), ints.end(), 0);
-  }
+    std::vector<int> ints(sepNumsAsString.size(), 0);
+    std::transform(sepNumsAsString.begin(), sepNumsAsString.end(),
+                   ints.begin(), &Convert::String::to<int>);
+
+    std::vector<int>::iterator p = std::partition(ints.begin(), ints.end(), std::bind2nd(std::less<int>(), 0));
+
+    if(p == ints.begin())
+    {
+        return std::accumulate(ints.begin(), ints.end(), 0);
+    }
+    else
+    {
+        std::vector<int> negNums;
+        std::copy(ints.begin(), p, std::back_inserter(negNums));
+
+        std::ostringstream exceptTxt;
+        exceptTxt << "Negative numbers not allowed: ";
+        std::copy(negNums.begin(), negNums.end(),
+                  std::ostream_iterator<int>(exceptTxt, " "));
+
+        throw std::invalid_argument(exceptTxt.str());
+    }
+}
 
 }
 
--- string_calculator/c++/test/main.cpp
+++ string_calculator/c++/test/main.cpp
@@ -178,10 +178,7 @@ TEST_CASE("add with one number string returns that number", "[add]")
 
 TEST_CASE("add with two nubers returns their sum", "[add]")
 {
-  CHECK(StringCalculator::add("0,0") == 0);
-  CHECK(StringCalculator::add("0,1") == 1);
-  CHECK(StringCalculator::add("1,1") == 2);
-  CHECK(StringCalculator::add("1,2") == 3);
+  CHECK(StringCalculator::add("1,22") == 23);
 
   int const max = std::numeric_limits<int>::max() - 1;
   int const halfMax = max/2;
@@ -193,49 +190,38 @@ TEST_CASE("add with two nubers returns their sum", "[add]")
 
 TEST_CASE("add with many numbers returns their sum", "[add]")
 {
-  CHECK(StringCalculator::add("0,0,0,0") == 0);
-  CHECK(StringCalculator::add("0,1,0,2") == 3);
-  CHECK(StringCalculator::add("1,2,3,4,5,6,7,8,9,0") == 45);
-  CHECK(StringCalculator::add("11,12,13,14,15") == 65);
-  CHECK(StringCalculator::add("1000000,1000000,1000000") == 3000000);
+  CHECK(StringCalculator::add("1,22,333") == 356);
 }
 
 TEST_CASE("add with many numbers and \\n as seperator returns their sum", "[add]")
 {
-  CHECK(StringCalculator::add("0\n0\n0\n0") == 0);
-  CHECK(StringCalculator::add("0\n1\n0\n2") == 3);
-  CHECK(StringCalculator::add("1\n2\n3\n4\n5\n6\n7\n8\n9\n0") == 45);
-  CHECK(StringCalculator::add("11\n12\n13\n14\n15") == 65);
-  CHECK(StringCalculator::add("1000000\n1000000\n1000000") == 3000000);
+  CHECK(StringCalculator::add("1\n22\n333") == 356);
 }
 
 TEST_CASE("add with many numbers and \\n or , as seperator returns their sum", "[add]")
 {
-  CHECK(StringCalculator::add("0\n0,0\n0") == 0);
-  CHECK(StringCalculator::add("0,1\n0,2") == 3);
-  CHECK(StringCalculator::add("1\n2\n3,4,5\n6\n7,8\n9\n0") == 45);
-  CHECK(StringCalculator::add("11,12\n13\n14,15") == 65);
-  CHECK(StringCalculator::add("1000000,1000000\n1000000") == 3000000);
+  CHECK(StringCalculator::add("1,22\n333") == 356);
 }
 
 TEST_CASE("add with seperator specification and many number returns their sum", "[add]")
 {
-  CHECK(StringCalculator::add("//\n\n0\n0\n0\n0") == 0);
-  CHECK(StringCalculator::add("//,\n0,1,22") == 23);
-  CHECK(StringCalculator::add("//\\\n11\\2\\3\\4\\5\\6\\7\\8\\9\\0") == 55);
-  CHECK(StringCalculator::add("//-\n11-12-13-14-15") == 65);
-  CHECK(StringCalculator::add("//:\n1000000:1000000:1000000") == 3000000);
+  CHECK(StringCalculator::add("//-\n1-22-333") == 356);
 }
 
 TEST_CASE("add with negative numbers throws", "[add]")
 {
   REQUIRE_THROWS_AS(StringCalculator::add("1,-1,1,-1"), std::invalid_argument);
+}
+
+TEST_CASE("add with negative numbers throws and exception text contains negative numbers", "[add]")
+{
   try
     {
-      StringCalculator::add("1,-1,1,-1");
+      StringCalculator::add("1,-1,22,-22,333,-333");
     }
   catch(std::invalid_argument const& e)
     {
-      CHECK(e.what() == std::string("Negative numbers not allowed: -1 -1 "));
+      //CHECK_THAT(e.what(), Catch::Contains("-1"));
     }
 }
+
