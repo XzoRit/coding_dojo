2a9b417c35b2654a922ac4fd1e8cbc0218eb6b19
XzoRit <xzorit@hotmail.com>
2014-02-08 16:25:08 +0100

    added support for renaming files and directories if they got snaked

--- case_it/bash/snake_it.sh
+++ case_it/bash/snake_it.sh
@@ -2,14 +2,15 @@
 
 set -o errexit
 set -o nounset
-set -x
 shopt -s extglob
+#set -x
 #set -o verbose
 
 snake_it() {
     for it in "${@}"; do
 	local replaced_with_underscore="${it//+([[:space:]]|[[:punct:]])/_}"
-	local different_cases_with_underscore=$( echo "${replaced_with_underscore}" | sed -e "s/\([a-z]\)\([A-Z]\)/\1_\2/g" )
+	local different_cases_with_underscore=$( echo "${replaced_with_underscore}" |\
+                                                 sed -e "s/\([a-z]\)\([A-Z]\)/\1_\2/g" )
 	echo "${different_cases_with_underscore,,}"
     done
 }
@@ -31,29 +32,27 @@ check() {
     fi
 }
 
-check_snake_it() {
-    check snake_it "${@}"
-}
-
-check_snake_it "replace space with _" "a_b_c_d" "a b c d"
-check_snake_it "collapse multiple spaces to one _" "a_b_c_d" "a b  c   d"
-check_snake_it "convert upper to lower" "a_b_c_d" "A B C D"
-check_snake_it "insert _ between camel cased letters" "a_b_c_d" "aB cD"
-check_snake_it "seperator . is replaced with _" "1_12_123_1234" "1.12.123.1234"
-check_snake_it "seperator - is replaced with _" "1_12_123_1234" "1-12-123-1234"
-check_snake_it "multiple texts are snaked with \\n as seperator" "a_b_c_d
+check snake_it "replace space with _" "a_b_c_d" "a b c d"
+check snake_it "collapse multiple spaces to one _" "a_b_c_d" "a b  c   d"
+check snake_it "convert upper to lower" "a_b_c_d" "A B C D"
+check snake_it "insert _ between camel cased letters" "a_b_c_d" "aB cD"
+check snake_it "seperator . is replaced with _" "1_12_123_1234" "1.12.123.1234"
+check snake_it "seperator - is replaced with _" "1_12_123_1234" "1-12-123-1234"
+check snake_it "multiple texts are snaked with \\n as seperator" "a_b_c_d
 a_b_c_d
 a_b_c_d
 a_b_c_d
 1_12_123_1234
 1_12_123_1234" "a b c d" "a b  c   d" "A B C D" "aB cD" "1.12.123.1234" "1-12-123-1234"
 
-touch "a b c d.txt"
-touch "a b  c   d.txt"
-touch "A B C D.txt"
-touch "aB cD.txt"
-touch "1.12.123.1234.txt"
-touch "1-12-123-1234.txt"
+# test renaming files
+touch "a b c d.xzr"
+touch "a b  c   d.xzr"
+touch "A B C D.xzr"
+touch "aB cD.xzr"
+touch "1.12.123.1234.xzr"
+touch "1-12-123-1234.xzr"
+mkdir "test 1 12 aGoodTest Xzr"
 for it in *; do
     extension=
     basename="${it}"
@@ -63,13 +62,9 @@ for it in *; do
     fi
     snaked=$( snake_it "${basename}" )
     snaked="${snaked}${extension}"
-    if [ "${snaked}" != "${it}" ]; then
-	printf "before = %s\nafter  = %s\n" "${it}" "${snaked}"
+    if [ "${snaked}" != "${it}" -a \( -f "${it}" -o -d "${it}" \) ]; then
+	mv "${it}" "${snaked}"
     fi
+    echo "${snaked}"
 done
-rm "a b c d.txt"
-rm "a b  c   d.txt"
-rm "A B C D.txt"
-rm "aB cD.txt"
-rm "1.12.123.1234.txt"
-rm "1-12-123-1234.txt"
+rm -d *xzr
