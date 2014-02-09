bbfc3fc59f0c13bd5192b571de240bac40f1e85f
XzoRit <xzorit@hotmail.com>
2014-02-09 16:58:28 +0100

    changed check function to accept name of return value of command to be checked

--- case_it/bash/snake_it.sh
+++ case_it/bash/snake_it.sh
@@ -21,11 +21,12 @@ xzr_snake_it() {
 
 xzr_check() {
     local command="${1}"
-    local check_name="${2}"
-    local expected="${3}"
-    shift 3
+    local return_value="${2}"
+    local check_name="${3}"
+    local expected="${4}"
+    shift 4
     "${command}" "${@}"
-    local actual="${XZR_SNAKED}"
+    local actual="${!return_value}"
     if [ "${expected}" != "${actual}" ]; then
 	{
 	    printf "FAILURE in \"%s\"\n" "${check_name}"
@@ -37,15 +38,15 @@ xzr_check() {
     fi
 }
 
-xzr_check xzr_snake_it "replace space with _" "a_b_c_d" "a b c d"
-xzr_check xzr_snake_it "collapse multiple spaces to one _" "a_b_c_d" "a b  c   d"
-xzr_check xzr_snake_it "convert upper to lower" "a_b_c_d" "A B C D"
-xzr_check xzr_snake_it "insert _ between camel cased letters" "a_b_c_d" "aB cD"
-xzr_check xzr_snake_it "seperator . is replaced with _" "1_12_123_1234" "1.12.123.1234"
-xzr_check xzr_snake_it "seperator - is replaced with _" "1_12_123_1234" "1-12-123-1234"
-xzr_check xzr_snake_it "multiple texts are snaked with a space as seperator" "a_b_c_d a_b_c_d a_b_c_d a_b_c_d 1_12_123_1234 1_12_123_1234" "a b c d" "a b  c   d" "A B C D" "aB cD" "1.12.123.1234" "1-12-123-1234"
+xzr_check xzr_snake_it XZR_SNAKED "replace space with _" "a_b_c_d" "a b c d"
+xzr_check xzr_snake_it XZR_SNAKED "collapse multiple spaces to one _" "a_b_c_d" "a b  c   d"
+xzr_check xzr_snake_it XZR_SNAKED "convert upper to lower" "a_b_c_d" "A B C D"
+xzr_check xzr_snake_it XZR_SNAKED "insert _ between camel cased letters" "a_b_c_d" "aB cD"
+xzr_check xzr_snake_it XZR_SNAKED "seperator . is replaced with _" "1_12_123_1234" "1.12.123.1234"
+xzr_check xzr_snake_it XZR_SNAKED "seperator - is replaced with _" "1_12_123_1234" "1-12-123-1234"
+xzr_check xzr_snake_it XZR_SNAKED "multiple texts are snaked with a space as seperator" "a_b_c_d a_b_c_d a_b_c_d a_b_c_d 1_12_123_1234 1_12_123_1234" "a b c d" "a b  c   d" "A B C D" "aB cD" "1.12.123.1234" "1-12-123-1234"
 
-# test renaming files
+# test renaming files/directories
 touch "a b c d.xzr"
 touch "a b  c   d e.xzr"
 touch "A B C Df.xzr"
