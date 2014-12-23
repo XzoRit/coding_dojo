df4bb9e141612320b21d8432031a776777e46073
XzoRit <xzorit@hotmail.com>
2014-12-23 22:16:37 +0100

    print results of foldLeft operation

--- string_calculator/scala/string_calculator.scala
+++ string_calculator/scala/string_calculator.scala
@@ -1,5 +1,3 @@
-"1,22,333".split(",").map(_.toInt).foldLeft(0)(_+_)
-
-"1,22,333".split(",").map{_.toInt}.foldLeft(0){_+_}
-
-"1,22,333".split(",").foldLeft(0)(_+_.toInt)
+println("1,22,333".split(",").map(_.toInt).foldLeft(0)(_+_))
+println("1,22,333".split(",").map{_.toInt}.foldLeft(0){_+_})
+println("1,22,333".split(",").foldLeft(0)(_+_.toInt))
