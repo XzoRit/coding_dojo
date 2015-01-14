7ed92f044b99ee1a2b1c88924289eb9e43030cf2
XzoRit <xzorit@hotmail.com>
2015-01-14 16:19:37 +0100

    added hint how to start script

--- string_calculator/scala/string_calculator.scala
+++ string_calculator/scala/string_calculator.scala
@@ -1,3 +1,4 @@
+/* run script with scala-2.11 <script-name> */
 println("1,22,333".split(",").map(_.toInt).foldLeft(0)(_+_))
 println("1,22,333".split(",").map{_.toInt}.foldLeft(0){_+_})
 println("1,22,333".split(",").foldLeft(0)(_+_.toInt))
