88dc696b7e71604d03f2b7f9b32e0260c938fd37
XzoRit <xzorit@hotmail.com>
2015-01-28 20:55:07 +0100

    indented test cases

--- string_calculator/d/source/string_calculator.d
+++ string_calculator/d/source/string_calculator.d
@@ -22,9 +22,19 @@ import specd.specd;
 
 unittest
 {
-  describe("calling add with an empty string").should("return 0", (add("")).must.equal(0));
-  describe("calling add with a number").should("return that number", (add("1234567890")).must.equal(1234567890));
-  describe("calling add with a comma separated list of numbers").should("return the sum of these numbers", (add("1,22,333")).must.equal(356));
-  describe("calling add with many numbers separated by comma or newline").should("return the sum of these numbers", (add("1\n22,333")).must.equal(356));
-  describe("calling add with numbers separated by a character contained in the separator specification section").should("return the sum of these numbers", (add("//T\n1T22T333")).must.equal(356));
+  describe("calling add with an empty string")
+    .should("return 0",
+	    (add("")).must.equal(0));
+  describe("calling add with a number")
+    .should("return that number",
+	    (add("1234567890")).must.equal(1234567890));
+  describe("calling add with a comma separated list of numbers")
+    .should("return the sum of these numbers",
+	    (add("1,22,333")).must.equal(356));
+  describe("calling add with many numbers separated by comma or newline")
+    .should("return the sum of these numbers",
+	    (add("1\n22,333")).must.equal(356));
+  describe("calling add with numbers separated by a character contained in the separator specification section")
+    .should("return the sum of these numbers",
+	    (add("//T\n1T22T333")).must.equal(356));
 }
