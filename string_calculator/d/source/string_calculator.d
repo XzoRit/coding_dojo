1a1f2d204e7169ba3127917d92ffbf617a2bd2c2
XzoRit <xzorit@hotmail.com>
2015-01-28 19:43:11 +0100

    just some indenting

--- string_calculator/d/source/string_calculator.d
+++ string_calculator/d/source/string_calculator.d
@@ -5,28 +5,28 @@ import std.algorithm;
 
 int add(string numbers)
 {
-	if(numbers.empty) return 0;
-	auto separator = [','];
-	if(numbers[0] == '/')
-	{
-		separator[0] = numbers[2];
-		numbers = numbers[4..$];
-	}
-	immutable replaced = replace(numbers, "\n", separator);
-	auto splitted = split(replaced, separator);
-	auto ints = map!(a => to!int(a))(splitted);
-	return reduce!((a, b) => a + b)(0, ints);
+  if(numbers.empty) return 0;
+  auto separator = [','];
+  if(numbers[0] == '/')
+    {
+      separator[0] = numbers[2];
+      numbers = numbers[4..$];
+    }
+  immutable replaced = replace(numbers, "\n", separator);
+  auto splitted = split(replaced, separator);
+  auto ints = map!(a => to!int(a))(splitted);
+  return reduce!((a, b) => a + b)(0, ints);
 }
 
 import specd.specd;
 
 unittest
 {
-	assert(add("") == 0);
-	assert(add("1") == 1);
-	assert(add("11") == 11);
-	assert(add("1,22") == 23);
-	assert(add("1,22,333") == 356);
-	assert(add("1\n22,333") == 356);
-	assert(add("//T\n1T22T333") == 356);
+  assert(add("") == 0);
+  assert(add("1") == 1);
+  assert(add("11") == 11);
+  assert(add("1,22") == 23);
+  assert(add("1,22,333") == 356);
+  assert(add("1\n22,333") == 356);
+  assert(add("//T\n1T22T333") == 356);
 }
