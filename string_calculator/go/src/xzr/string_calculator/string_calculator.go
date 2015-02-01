21800dc4cd38a26ab57451304e588ff93fcefca4
XzoRit <xzorit@hotmail.com>
2015-02-01 22:24:48 +0100

    add with comma separated numbers returns sum

--- string_calculator/go/src/xzr/string_calculator/string_calculator.go
+++ string_calculator/go/src/xzr/string_calculator/string_calculator.go
@@ -1,11 +1,18 @@
 package string_calculator
 
-import "strconv"
+import (
+	"strconv"
+	"strings"
+)
 
-func Add(numbers string) (num int, err error) {
-	num, err = strconv.Atoi(numbers)
-	if err != nil {
-		return
+func Add(numbers string) (sum int, err error) {
+	splitted := strings.Split(numbers, ",")
+	for _, n := range splitted {
+		num, e := strconv.Atoi(n)
+		if e != nil {
+			return
+		}
+		sum += num
 	}
-	return num, nil
+	return sum, nil
 }
--- string_calculator/go/src/xzr/string_calculator/string_calculator_test.go
+++ string_calculator/go/src/xzr/string_calculator/string_calculator_test.go
@@ -6,8 +6,8 @@ import (
 )
 
 func TestAddWithEmptyStringReturns0(t *testing.T) {
-	actual, _ := Add("")
 	expected := 0
+	actual, _ := Add("")
 	if actual !=  expected {
 		t.Errorf("Expected: %d Actual %d", expected, actual)
 	}
@@ -16,6 +16,14 @@ func TestAddWithEmptyStringReturns0(t *testing.T) {
 func TestAddWithANumberShouldReturnThatNumber(t *testing.T) {
 	expected := 1234567890
 	actual, _ := Add(strconv.Itoa(expected))
+	if actual !=  expected {
+		t.Errorf("Expected: %d Actual %d", expected, actual)
+	}
+}
+
+func TestAddWithCommaSeparatedNumberShouldReturnTheSum(t *testing.T) {
+	expected := 356
+	actual, _ := Add("1,22,333")
 	if actual !=  expected {
 		t.Errorf("Expected: %d Actual %d", expected, actual)
 	}
