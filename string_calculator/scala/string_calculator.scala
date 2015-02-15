374b96a9b4c427f0a0a25fbbb1ec0d7be62dcc56
XzoRit <xzorit@hotmail.com>
2015-02-15 10:56:01 +0100

    extractNumber now returns sequence of numbers as string

--- string_calculator/scala/string_calculator.scala
+++ string_calculator/scala/string_calculator.scala
@@ -7,17 +7,18 @@ class StringCalculator {
   def extractNumbers(numbers: String) = {
     if(numbers.startsWith("//")) {
       val endOfSepSpec = numbers.indexOf("\n")
-      (numbers slice(2, endOfSepSpec), numbers.drop(endOfSepSpec + 1))
+      val separator = numbers slice(2, endOfSepSpec)
+      val numsWithoutSepSpec = numbers.drop(endOfSepSpec + 1)
+      separator.r.split(numsWithoutSepSpec)
     }
     else {
-      (""",|\n""", numbers)
+      ",".r.split(numbers.replaceAll("\n", ","))
     }
   }
 
   def add(numbers: String) = {
     if(numbers.isEmpty) 0 else {
-      val (seperator, nums) = extractNumbers(numbers)
-      val (negs, pos) = seperator.r.split(nums).map{_.toInt}.partition{_ < 0}
+      val (negs, pos) = extractNumbers(numbers).map{_.toInt}.partition{_ < 0}
       if(negs.isEmpty) pos.foldLeft(0){_ + _} else {	
 	throw new IllegalArgumentException(negs.map{_.toString}.foldLeft("Negative numbers not allowed: "){_ ++ _ ++ " "})
       }
@@ -42,18 +43,15 @@ class TestStringCalculator extends FlatSpec with Matchers with BeforeAndAfter {
   }
 
   "numbers seperated with comma or newline" should "be summed up" in {
-    calculator.extractNumbers("1,22\n333") should be (""",|\n""", "1,22\n333")
     calculator.add("1,22\n333") should be (356)
   }
 
   "if seperator specification is present add" should "use the character as seperator" in {
-    calculator.extractNumbers("//;\n1;22;333") should be (";", "1;22;333")
     calculator.add("//;\n1;22;333") should be (356)
   }
 
   "if seperator specification is present add" should "use the string as seperator" in {
-    calculator.extractNumbers("//-_-\n1-_-22-_-333") should be ("-_-", "1-_-22-_-333")
-    calculator.add("//-_-\n1-_-22-_-333") should be (356)
+    calculator.add("//-T-\n1-T-22-T-333") should be (356)
   }
   
   "add with negative numbers" should "throw an exception with negative numbers listed" in {
