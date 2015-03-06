dc3b4cc79e8f8e858c7039d499b76840638b4ca0
XzoRit <xzorit@hotmail.com>
2015-03-06 15:04:12 +0100

    added TeaReipe

--- coffee_machine/d/source/coffee_machine.d
+++ coffee_machine/d/source/coffee_machine.d
@@ -20,6 +20,26 @@ class CoffeeRecipe : Recipe
   }
 }
 
+class TeaRecipe : Recipe
+{
+  int amountWaterMl()
+  {
+    return 200;
+  }
+
+  unittest
+    {
+      describe("amount water for tea")
+	.should("be 200ml",
+		(new TeaRecipe().amountWaterMl().must.equal(200)));
+    }
+
+  void brew()
+  {
+    writeln("steeping Tea");
+  }
+}
+
 unittest
 {
   describe("amount water for coffee")
