d083849717c5cf7defc4da9f1fd47165c2fa5fbc
XzoRit <xzorit@hotmail.com>
2015-04-07 20:32:43 +0200

    importing string calculator in test module

--- string_calculator/elixir/string_calculator/test/string_calculator_test.exs
+++ string_calculator/elixir/string_calculator/test/string_calculator_test.exs
@@ -1,19 +1,20 @@
 defmodule StringCalculatorTest do
   use ExUnit.Case
+	import StringCalculator
 
   test "empty string returns 0" do
-    assert StringCalculator.add("") == 0
+    assert add("") == 0
   end
 
 	test "string with one number returns that number" do
-		assert StringCalculator.add("1234567890") == 1234567890
+		assert add("1234567890") == 1234567890
 	end
 
 	test "string with comma separated numbers returns the sum" do
-		assert StringCalculator.add("1,22,333") == 356
+		assert add("1,22,333") == 356
 	end
 
 	test "numbers can be separeted by comma or newline" do
-		assert StringCalculator.add("0\n1,22\n333") == 356
+		assert add("0\n1,22\n333") == 356
 	end
 end
