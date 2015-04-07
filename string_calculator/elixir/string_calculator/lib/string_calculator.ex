4e5a4cb1b8815b48cf6a8495dbd0a27a3e273c7a
XzoRit <xzorit@hotmail.com>
2015-04-07 20:38:34 +0200

    each pipe operator starts in a new line

--- string_calculator/elixir/string_calculator/lib/string_calculator.ex
+++ string_calculator/elixir/string_calculator/lib/string_calculator.ex
@@ -2,7 +2,10 @@ defmodule StringCalculator do
 	def add (numsAsString) do
 		case String.length(numsAsString) do
 			0 ->	0
-			_ -> String.split(String.replace(numsAsString, "\n", ","), ",") |> Enum.map(&(String.to_integer/1)) |> Enum.reduce(&(+/2))
+			_ -> String.replace(numsAsString, "\n", ",")
+					 |> String.split(",")
+					 |> Enum.map(&(String.to_integer/1))
+					 |> Enum.reduce(&(+/2))
 		end
 	end
 end
