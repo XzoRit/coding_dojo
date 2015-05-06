dde2865eff76cc7b19af17ad2b136e313e84e59d
XzoRit <xzorit@hotmail.com>
2015-05-06 21:33:54 +0200

    appholders one day passed method renamed

--- gilded_rose/c++/test.cpp
+++ gilded_rose/c++/test.cpp
@@ -18,7 +18,7 @@ public:
     return items.front().quality;
   }
 
-  void oneDayPassed()
+  void updateQuality()
   {
     app.updateQuality();
   }
@@ -44,9 +44,9 @@ SCENARIO("days pass for an item")
     {
       Item const item("item", 1, 2);
       AppHolder app(item);
-      WHEN("one day passes")
+      WHEN("quality is updated")
 	{
-	  app.oneDayPassed();
+	  app.updateQuality();
 	  THEN("quality value is decremented by one")
 	    {
 	      CHECK(app.itemQuality() == 1);
@@ -55,15 +55,22 @@ SCENARIO("days pass for an item")
       WHEN("sellin value is 0")
 	{
 	  app.setSellInTo(0);
-	  app.oneDayPassed();
-	  THEN("quality value is decremented by two")
+	  AND_WHEN("quality is updated")
 	    {
-	      CHECK(app.itemQuality() == 0);
+	      app.updateQuality();
+	      THEN("quality value is decremented by two")
+		{
+		  CHECK(app.itemQuality() == 0);
+		}
 	    }
-	  AND_WHEN("another day passes")
+	}
+      WHEN("quality value is 0")
+	{
+	  app.setQualityTo(0);
+	  AND_WHEN("quality is updated")
 	    {
-	      app.oneDayPassed();
-	      THEN("quality value does not become negativ")
+	      app.updateQuality();
+	      THEN("quality value does change")
 		{
 		  CHECK(app.itemQuality() == 0);
 		}
@@ -78,9 +85,9 @@ SCENARIO("days pass for aged brie")
     {
       Item const item("Aged Brie", 1, 17);
       AppHolder app(item);
-      WHEN("one day passes")
+      WHEN("quality is updated")
 	{
-	  app.oneDayPassed();
+	  app.updateQuality();
 	  THEN("quality increases by one")
 	    {
 	      CHECK(app.itemQuality() == 18);
@@ -89,11 +96,13 @@ SCENARIO("days pass for aged brie")
       WHEN("sellin value is 0")
 	{
 	  app.setSellInTo(0);
-	  CAPTURE(app.itemQuality());
-	  app.oneDayPassed();
-	  THEN("quality increases by one")
+	  AND_WHEN("quality is updated")
 	    {
-	      CHECK(app.itemQuality() == 18);
+	      app.updateQuality();
+	      THEN("quality increases by one")
+		{
+		  CHECK(app.itemQuality() == 18);
+		}
 	    }
 	}
       WHEN("quality value is 50")
@@ -101,7 +110,7 @@ SCENARIO("days pass for aged brie")
 	  app.setQualityTo(50);
 	  AND_WHEN("quality is updated")
 	    {
-	      app.oneDayPassed();
+	      app.updateQuality();
 	      THEN("quality value does not change")
 		{
 		  CHECK(app.itemQuality() == 50);
@@ -113,13 +122,13 @@ SCENARIO("days pass for aged brie")
 
 SCENARIO("days pass for backstage pass")
 {
-  GIVEN("a backstage pass")
+  GIVEN("a backstage pass with positive sellin value")
     {
       Item const item("Backstage pass for ac/dc", 11, 47);
       AppHolder app(item);
-      WHEN("one day passes")
+      WHEN("quality is updated")
 	{
-	  app.oneDayPassed();
+	  app.updateQuality();
 	  THEN("quality increases by one")
 	    {
 	      CHECK(app.itemQuality() == 48);
@@ -128,24 +137,34 @@ SCENARIO("days pass for backstage pass")
       WHEN("sellin value is equal to 10")
 	{
 	  app.setSellInTo(10);
-	  app.oneDayPassed();
-	  THEN("quality increaes by two")
+	  AND_WHEN("quality is updated")
 	    {
-	      CHECK(app.itemQuality() == 49);
+	      app.updateQuality();
+	      THEN("quality increaes by two")
+		{
+		  CHECK(app.itemQuality() == 49);
+		}
 	    }
 	}
       WHEN("sellin value is equal to 5")
 	{
 	  app.setSellInTo(5);
-	  app.oneDayPassed();
-	  THEN("quality increases by 3")
+	  AND_WHEN("quality is updated")
 	    {
-	      CHECK(app.itemQuality() == 50);
+	      app.updateQuality();
+	      THEN("quality increases by 3")
+		{
+		  CHECK(app.itemQuality() == 50);
+		}
 	    }
-	  AND_WHEN("another day passes")
+	}
+      WHEN("quality value is 50")
+	{
+	  app.setQualityTo(50);
+	  AND_WHEN("quality is updated")
 	    {
-	      app.oneDayPassed();
-	      THEN("quality value does not become greater than 50")
+	      app.updateQuality();
+	      THEN("quality value does not change")
 		{
 		  CHECK(app.itemQuality() == 50);
 		}
@@ -154,17 +173,24 @@ SCENARIO("days pass for backstage pass")
       WHEN("cancert is over")
 	{
 	  app.setSellInTo(0);
-	  app.oneDayPassed();
-	  THEN("quality value is 0")
+	  AND_WHEN("quality is updated")
 	    {
-	      CHECK(app.itemQuality() == 0);
-	    }
-	  AND_WHEN("another day passes")
-	    {
-	      app.oneDayPassed();
-	      THEN("quality value does not become negative")
+	      app.updateQuality();
+	      THEN("quality value is 0")
 		{
 		  CHECK(app.itemQuality() == 0);
+		}
+	    }
+	  AND_WHEN("quality value is 0")
+	    {
+	      app.setQualityTo(0);
+	      AND_WHEN("quality is updated")
+		{
+		  app.updateQuality();
+		  THEN("quality value does not change")
+		    {
+		      CHECK(app.itemQuality() == 0);
+		    }
 		}
 	    }
 	}
