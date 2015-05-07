bf71339a18c505659358e507098985dabe2260b8
XzoRit <xzorit@hotmail.com>
2015-05-07 13:51:49 +0200

    using iterator to loop through items

--- gilded_rose/c++/gilded_rose.cpp
+++ gilded_rose/c++/gilded_rose.cpp
@@ -9,67 +9,67 @@ GildedRose::GildedRose(vector<Item>& items)
     
 void GildedRose::updateQuality() 
 {
-  for (int i = 0; i < items.size(); i++)
+  for (vector<Item>::iterator it = items.begin(); it < items.end(); ++it)
     {
-      if (items[i].name != "Aged Brie" && items[i].name.substr(0, 14) != "Backstage pass")
+      if (it->name != "Aged Brie" && it->name.substr(0, 14) != "Backstage pass")
         {
-          if (items[i].quality > Quality::min())
+          if (it->quality > Quality::min())
             {
-              if (items[i].name.substr(0, 8) != "Sulfuras")
+              if (it->name.substr(0, 8) != "Sulfuras")
                 {
-                  items[i].quality = items[i].quality - 1;
+                  it->quality = it->quality - 1;
                 }
             }
         }
       else
         {
-          if (items[i].quality < Quality::max())
+          if (it->quality < Quality::max())
             {
-              items[i].quality = items[i].quality + 1;
+              it->quality = it->quality + 1;
 
-              if (items[i].name.substr(0, 14) == "Backstage pass")
+              if (it->name.substr(0, 14) == "Backstage pass")
                 {
-                  if (items[i].sellIn < 11)
+                  if (it->sellIn < 11)
                     {
-                      if (items[i].quality < Quality::max())
+                      if (it->quality < Quality::max())
                         {
-                          items[i].quality = items[i].quality + 1;
+                          it->quality = it->quality + 1;
                         }
                     }
 
-                  if (items[i].sellIn < 6)
+                  if (it->sellIn < 6)
                     {
-                      if (items[i].quality < Quality::max())
+                      if (it->quality < Quality::max())
                         {
-                          items[i].quality = items[i].quality + 1;
+                          it->quality = it->quality + 1;
                         }
                     }
                 }
             }
         }
 
-      if (items[i].name.substr(0, 8) != "Sulfuras")
+      if (it->name.substr(0, 8) != "Sulfuras")
         {
-          items[i].sellIn = items[i].sellIn - 1;
+          it->sellIn = it->sellIn - 1;
         }
 
-      if (items[i].sellIn < 0)
+      if (it->sellIn < 0)
         {
-          if (items[i].name != "Aged Brie")
+          if (it->name != "Aged Brie")
             {
-              if (items[i].name.substr(0, 14) != "Backstage pass")
+              if (it->name.substr(0, 14) != "Backstage pass")
                 {
-                  if (items[i].quality > Quality::min())
+                  if (it->quality > Quality::min())
                     {
-                      if (items[i].name.substr(0, 8) != "Sulfuras")
+                      if (it->name.substr(0, 8) != "Sulfuras")
                         {
-                          items[i].quality = items[i].quality - 1;
+                          it->quality = it->quality - 1;
                         }
                     }
                 }
               else
                 {
-                  items[i].quality = items[i].quality - items[i].quality;
+                  it->quality = it->quality - it->quality;
                 }
             }
         }
