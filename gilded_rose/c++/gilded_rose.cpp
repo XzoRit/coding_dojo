bdacbaea8d064729960f074ce3dc42beeca9a1b7
XzoRit <xzorit@hotmail.com>
2015-05-08 15:50:15 +0200

    created update functions for each item type

--- gilded_rose/c++/gilded_rose.cpp
+++ gilded_rose/c++/gilded_rose.cpp
@@ -22,60 +22,80 @@ GildedRose::GildedRose(vector<Item>& items)
   : items(items)
 {}
     
+static void updateSulfuras(Item& it)
+{
+}
+
+static void updateAgedBrie(Item& it)
+{
+  if (it.quality < Quality::max())
+    {
+      ++it.quality;
+    }
+  --it.sellIn;
+}
+
+static void updateBackstagePass(Item& it)
+{
+  if (it.sellIn <= 0)
+    {
+      it.quality = Quality::min();
+    }
+  else if (it.sellIn < 6)
+    {
+      it.quality += 3;
+    }
+  else if (it.sellIn < 11)
+    {
+      it.quality += 2;
+    }
+  else
+    {
+      ++it.quality;
+    }
+  if (it.quality > Quality::max())
+    {
+      it.quality = Quality::max();
+    }
+  --it.sellIn;
+}
+
+static void updateItem(Item& it)
+{
+  if (it.sellIn > 0)
+    {
+      --it.quality;
+    }
+  else
+    {
+      it.quality -= 2;
+    }
+  if (it.quality < Quality::min())
+    {
+      it.quality = Quality::min();
+    }
+  --it.sellIn;
+}
+
 void GildedRose::updateQuality() 
 {
   for (vector<Item>::iterator it = items.begin(); it < items.end(); ++it)
     {
       if (isSulfuras(*it))
         {
+	  updateSulfuras(*it);
         }
       else if (isAgedBrie(*it))
         {
-          if (it->quality < Quality::max())
-            {
-              ++it->quality;
-	    }
-          --it->sellIn;
+	  updateAgedBrie(*it);
         }
       else if (isBackstagePass(*it))
         {
-	  if (it->sellIn <= 0)
-	    {
-	      it->quality = Quality::min();
-	    }
-	  else if (it->sellIn < 6)
-	    {
-	      it->quality += 3;
-	    }
-	  else if (it->sellIn < 11)
-	    {
-	      it->quality += 2;
-	    }
-	  else
-	    {
-	      ++it->quality;
-	    }
-          if (it->quality > Quality::max())
-            {
-	      it->quality = Quality::max();
-	    }
-          --it->sellIn;
+	  updateBackstagePass(*it);
 	}
       else
         {
-	  if (it->sellIn > 0)
-	    {
-	      --it->quality;
-	    }
-          else
-            {
-	      it->quality -= 2;
-	    }
-	  if (it->quality < Quality::min())
-	    {
-	      it->quality = Quality::min();
-	    }
-          --it->sellIn;
+	  updateItem(*it);
         }
     }
 }
