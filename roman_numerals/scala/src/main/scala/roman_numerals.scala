e82bf704ccdcb6897c3633905b5ee66d219c098e
XzoRit <xzorit@hotmail.com>
2015-11-24 09:04:57 +0100

    looping over symbol table

--- roman_numerals/scala/src/main/scala/roman_numerals.scala
+++ roman_numerals/scala/src/main/scala/roman_numerals.scala
@@ -5,21 +5,14 @@ class ArabicToRoman {
   def convert(arabic: Int) = {
     var roman = ""
     var count = arabic
-    while(count >= symbolTable(2)._1) {
-      roman += symbolTable(2)._2
-      count -= symbolTable(2)._1
+    symbolTable foreach { arabicRomanPair =>
+      while(count >= arabicRomanPair._1) {
+	roman += arabicRomanPair._2
+	count -= arabicRomanPair._1
+      }
     }
-    if(count >= symbolTable(1)._1) {
-      roman += symbolTable(1)._2
-      count -= symbolTable(1)._1
-    }
-    while(count >= symbolTable(0)._1)  {
-      roman += symbolTable(0)._2
-      count -= symbolTable(0)._1
-    }
-
     roman
   }
 
-  val symbolTable = List((1 -> "I"), (5 -> "V"), (10 -> "X"))
+  val symbolTable = List((10 -> "X"), (5 -> "V"), (1 -> "I"))
 }
