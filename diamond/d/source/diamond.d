54eb05f2c3ed3f29f0e6b9ab36da54ec123191cd
XzoRit <xzorit@hotmail.com>
2016-01-13 13:43:46 +0100

    spaces after each letter in line are calculated

--- diamond/d/source/diamond.d
+++ diamond/d/source/diamond.d
@@ -1,11 +1,22 @@
 module xzr.diamond;
 
+import std.ascii               : uppercase;
+import std.array               : array, replicate;
+import std.algorithm.iteration : map;
+import std.range               : iota;
+
 struct Diamond
 {
   static auto letters(char including)
   {
-    import std.ascii : uppercase;
     return uppercase[0..(including - 'A' + 1)];
+  }
+
+  static auto spaces_after(int num)
+  {
+    return iota(0, num+1)
+      .map!(a => replicate(" ", a))
+      .array;
   }
 }
 
@@ -14,11 +25,20 @@ unittest
   import specd.specd;
 
   describe("letters")
-    .should(["return A on A":
+    .should(["return A if given A":
 	     { Diamond.letters('A').must.equal("A"); },
-	     "return AB on B":
+	     "return AB if given B":
 	     { Diamond.letters('B').must.equal("AB"); },
-	     "return ABC on C":
+	     "return ABC if given C":
 	     { Diamond.letters('C').must.equal("ABC"); }
+	     ]);
+
+  describe("spaces_after")
+    .should(["""return [""] if given 0""":
+	     { Diamond.spaces_after(0).must.equal([""]); },
+	     """return ["", " "] if given 1""":
+	     { Diamond.spaces_after(1).must.equal(["", " "]); },
+	     """return ["", " ", "  "] if given 2""":
+	     { Diamond.spaces_after(2).must.equal(["", " ", "  "]); }
 	     ]);
 }
