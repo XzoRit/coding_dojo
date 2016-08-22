f62a16c2fcb3267b6dbb786d400fc90c38cb5284
XzoRit <xzorit@hotmail.com>
2016-08-22 20:46:31 +0200

    added test and lib for v3 implementation

--- diamond/c++/diamond.cpp
+++ diamond/c++/diamond.cpp
@@ -1,5 +1,6 @@
 #include "v1/diamond.hpp"
 #include "v2/diamond.hpp"
+#include "v3/diamond.hpp"
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
 #include <iostream>
@@ -148,6 +149,75 @@ namespace v2
   }
 }
 
+namespace v3
+{
+  namespace test
+  {
+    TEST_CASE("v3: letters")
+    {
+      CHECK(Diamond::letters(1) == "A");
+      CHECK(Diamond::letters(2) == "AB");
+      CHECK(Diamond::letters(3) == "ABC");
+    }
+
+    TEST_CASE("v3: spaces_after")
+    {
+      CHECK(Diamond::spaces_after(1) == (Diamond::Lines{""}));
+      CHECK(Diamond::spaces_after(2) == (Diamond::Lines{"", " "}));
+      CHECK(Diamond::spaces_after(3) == (Diamond::Lines{"", " ", "  "}));
+    }
+
+    TEST_CASE("v3: spaces_before")
+    {
+      CHECK(Diamond::spaces_before(1) == (Diamond::Lines{""}));
+      CHECK(Diamond::spaces_before(2) == (Diamond::Lines{" ", ""}));
+      CHECK(Diamond::spaces_before(3) == (Diamond::Lines{"  ", " ", ""}));
+    }
+
+    TEST_CASE("v3: lines")
+    {
+      CHECK(Diamond::lines(1) == (Diamond::Lines{"A"}));
+      CHECK(Diamond::lines(2) == (Diamond::Lines{" A", "B "}));
+      CHECK(Diamond::lines(3) == (Diamond::Lines{"  A", " B ", "C  "}));
+    }
+
+    TEST_CASE("v3: mirror_vertical")
+    {
+      CHECK(Diamond::mirror_vertical(Diamond::lines(1)) == (Diamond::Lines{"A"}));
+      CHECK(Diamond::mirror_vertical(Diamond::lines(2)) == (Diamond::Lines{" A ", "B B"}));
+      CHECK(Diamond::mirror_vertical(Diamond::lines(3)) == (Diamond::Lines{"  A  ", " B B ", "C   C"}));
+    }
+
+    TEST_CASE("v3: mirror_horizontal")
+    {
+      CHECK(Diamond::mirror_horizontal(Diamond::lines(1)) == (Diamond::Lines{"A"}));
+      CHECK(Diamond::mirror_horizontal(Diamond::lines(2)) == (Diamond::Lines{" A", "B ", " A"}));
+      CHECK(Diamond::mirror_horizontal(Diamond::lines(3)) == (Diamond::Lines{"  A", " B ", "C  ", " B ", "  A"}));
+    }
+
+    TEST_CASE("v3: all_lines")
+    {
+      CHECK(Diamond::all_lines(1) == (Diamond::Lines{"A"}));
+      CHECK(Diamond::all_lines(2) == (Diamond::Lines{" A ", "B B", " A "}));
+      CHECK(Diamond::all_lines(3) == (Diamond::Lines{"  A  ", " B B ", "C   C", " B B ", "  A  "}));
+    }
+
+    TEST_CASE("v3: char_to_num")
+    {
+      CHECK(Diamond::char_to_num('A') == 1);
+      CHECK(Diamond::char_to_num('B') == 2);
+      CHECK(Diamond::char_to_num('C') == 3);
+    }
+
+    TEST_CASE("v3: print")
+    {
+      CHECK(Diamond::print('A') == "A\n");
+      CHECK(Diamond::print('B') == " A \nB B\n A \n");
+      CHECK(Diamond::print('C') == "  A  \n B B \nC   C\n B B \n  A  \n");
+    }
+  }
+}
+
 int main(int argc, char *argv[])
 {
   int result = Catch::Session().run(argc, argv);
@@ -159,8 +229,9 @@ int main(int argc, char *argv[])
       
       for(const auto& c : "ABCDEFGHIJKLMNOPQRSTUVWXYZ"s)
       	{
-      	  cout << v1::Diamond::print(c) << '\n';
-      	  cout << v2::Diamond::print(c) << '\n';
+      	  // cout << v1::Diamond::print(c) << '\n';
+      	  // cout << v2::Diamond::print(c) << '\n';
+      	  // cout << v3::Diamond::print(c) << '\n';
       	}
     }
 
