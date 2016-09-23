1831c0be350c56d29dd4d732d22e509af1eb0bbf
XzoRit <xzorit@hotmail.com>
2016-09-23 17:07:46 +0200

    formating

--- csv/c++/main.cpp
+++ csv/c++/main.cpp
@@ -5,31 +5,35 @@
 
 namespace xzr::csv
 {
-  namespace impl
-  {
-  const auto split_by = [](const auto& text, const auto& sep)
-  {
-    return ranges::action::split(text, sep);
-  };
-  }
-  
-  const auto rows_from = [](const auto& csv_text)
-  {
+namespace impl
+{
+using ranges::action::split;
+const auto split_by = [](const auto& text, const auto& sep)
+{
+    return split(text, sep);
+};
+}
+
+const auto rows_from = [](const auto& csv_text)
+{
     return impl::split_by(csv_text, '\n');
-  };
+};
 }
 
 using namespace std::string_literals;
 
 TEST_CASE("split text by newline")
 {
-  const auto csv_text =
-    "first row\n"
-    "second row\n"
-    "third row"s;
-  const auto rows = xzr::csv::rows_from(csv_text);
-  REQUIRE(rows.size() == 3);
-  CHECK(rows[0] == "first row");
-  CHECK(rows[1] == "second row");
-  CHECK(rows[2] == "third row");
+    using xzr::csv::rows_from;
+    const auto csv_text =
+        "first row\n"
+        "second row\n"
+        "third row"s;
+
+    const auto rows = xzr::csv::rows_from(csv_text);
+
+    REQUIRE(rows.size() == 3);
+    CHECK(rows[0] == "first row");
+    CHECK(rows[1] == "second row");
+    CHECK(rows[2] == "third row");
 }
