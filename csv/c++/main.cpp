88cb1b4cf6317418387cb43eb8ef35b9fe1353c2
XzoRit <xzorit@hotmail.com>
2016-09-25 12:20:40 +0200

    splitted largest string per colum calculation into separated functions

--- csv/c++/main.cpp
+++ csv/c++/main.cpp
@@ -1,4 +1,5 @@
 #include <range/v3/action/split.hpp>
+#include <range/v3/numeric/accumulate.hpp>
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest.h>
 #include <string>
@@ -35,31 +36,55 @@ const auto csv_from = [](const auto& txt)
     }
     return v;
 };
-    const auto print = [](auto& str, const auto& data) -> decltype(auto)
+const auto by_colums = [](const auto& data)
+{
+    using namespace std;
+    vector<vector<string>> cols;
+    for(size_t col = 0; col < data[0].size(); ++col)
     {
-	using namespace std;
-	vector<size_t> col_sizes{};
-	for(size_t col = 0; col < data[0].size(); ++col)
-	{
-	    unsigned max{0};
-	    for(size_t row = 0; row < data.size(); ++row)
-	    {
-		max = (max > data[row][col].size()) ? max : data[row][col].size();
-	    }
-	    col_sizes.push_back(max);
-	}
-	for(auto&& r : data)
-	{
-	    size_t max_idx{0};
-	    for(auto&& c : r)
-	    {
-		str << c << string(col_sizes[max_idx] - c.size(), ' ') << '|';
-		++max_idx;
-	    }
-	    str << '\n';
-	}
-	return str;
-    };
+        vector<string> col_data;
+        for(size_t row = 0; row < data.size(); ++row)
+        {
+            col_data.push_back(data[row][col]);
+        }
+        cols.push_back(col_data);
+    }
+    return cols;
+};
+const auto max_size_of_each_col = [](const auto& data)
+{
+    using namespace std;
+    vector<size_t> col_sizes;
+    for(const auto& c : by_colums(data))
+    {
+        const auto m = ranges::accumulate(
+                           c,
+                           size_t{0},
+                           [](const auto& a, const auto& b)
+        {
+            return max(a, b.size());
+        });
+        col_sizes.push_back(m);
+    }
+
+    return col_sizes;
+};
+const auto print = [](auto& str, const auto& data) -> decltype(auto)
+{
+    using namespace std;
+    const auto col_sizes = max_size_of_each_col(data);
+    for(auto&& r : data)
+    {
+        size_t max_idx{0};
+        for(auto&& c : r)
+        {
+            str << c << string(col_sizes[max_idx] - c.size(), ' ') << '|';
+            ++max_idx;
+        }
+        str << '\n';
+    }
+    return str;
+};
 }
 
 using namespace std::string_literals;
@@ -76,7 +101,7 @@ TEST_CASE("csv to table")
         "Peter Pan;Am Hang 5;12345 Einsam;42\n"
         "Maria Schmitz;Koelner Strasse 45;50123 Koeln;43\n"
         "Paul Meier;Muenchener Weg 1;87654 Muenchen;65\n"
-	"Franz Kolp;Lange Strsse 15;45276 Holsten;18\n"s;
+        "Franz Kolp;Lange Strsse 15;45276 Holsten;18\n"s;
 
     const auto expected = vector<vector<string>>
     {
@@ -84,17 +109,17 @@ TEST_CASE("csv to table")
         {"Peter Pan","Am Hang 5","12345 Einsam","42"},
         {"Maria Schmitz","Koelner Strasse 45","50123 Koeln","43"},
         {"Paul Meier","Muenchener Weg 1","87654 Muenchen","65"},
-	{"Franz Kolp","Lange Strsse 15","45276 Holsten","18"}
+        {"Franz Kolp","Lange Strsse 15","45276 Holsten","18"}
     };
-    const auto actual = vector<vector<string>>{csv_from(csv)};
+    const auto actual = vector<vector<string>> {csv_from(csv)};
     CHECK(expected == actual);
 
     std::stringstream str;
     print(str, actual);
     CHECK(str.str() ==
-	  "Name         |Strasse           |Ort           |Alter|\n"
-	  "Peter Pan    |Am Hang 5         |12345 Einsam  |42   |\n"
-	  "Maria Schmitz|Koelner Strasse 45|50123 Koeln   |43   |\n"
-	  "Paul Meier   |Muenchener Weg 1  |87654 Muenchen|65   |\n"
-	  "Franz Kolp   |Lange Strsse 15   |45276 Holsten |18   |\n");
+          "Name         |Strasse           |Ort           |Alter|\n"
+          "Peter Pan    |Am Hang 5         |12345 Einsam  |42   |\n"
+          "Maria Schmitz|Koelner Strasse 45|50123 Koeln   |43   |\n"
+          "Paul Meier   |Muenchener Weg 1  |87654 Muenchen|65   |\n"
+          "Franz Kolp   |Lange Strsse 15   |45276 Holsten |18   |\n");
 }
