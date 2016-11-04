1b1fadecca753eade2ecb7ad0ccf79e0605e2f8a
XzoRit <xzorit@hotmail.com>
2016-11-04 17:12:44 +0100

    only player two scores

--- tennis/c++/tennis.cpp
+++ tennis/c++/tennis.cpp
@@ -10,18 +10,13 @@ string score(int a, int b)
   vector<string> numbers =
     {"Love", "Fifteen", "Thirty", "Fourty"};
   if(a) return numbers[a] + "-Love";
-  if(b) return "Love-Fifteen";
+  if(b) return "Love-" + numbers[b];
   return "Love-All";
 }
 
 TEST_CASE("score of 0,0")
 {
   CHECK(score(0, 0) == "Love-All");
-}
-
-TEST_CASE("score of 0,1")
-{
-  CHECK(score(0, 1) == "Love-Fifteen");
 }
 
 TEST_CASE("score of 1,0")
@@ -37,4 +32,19 @@ TEST_CASE("score of 2,0")
 TEST_CASE("score of 3,0")
 {
   CHECK(score(3, 0) == "Fourty-Love");
+}
+
+TEST_CASE("score of 0,1")
+{
+  CHECK(score(0, 1) == "Love-Fifteen");
+}
+
+TEST_CASE("score of 0,2")
+{
+  CHECK(score(0, 2) == "Love-Thirty");
+}
+
+TEST_CASE("score of 0,3")
+{
+  CHECK(score(0, 3) == "Love-Fourty");
 }
