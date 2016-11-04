9927b68e00e282c4d5d3d146354f9b11a3888a5e
XzoRit <xzorit@hotmail.com>
2016-11-04 15:56:05 +0100

    score of 1 0 is implemented

--- tennis/c++/tennis.cpp
+++ tennis/c++/tennis.cpp
@@ -7,9 +7,9 @@ using namespace std::string_literals;
 
 string score(int a, int b)
 {
-  auto result = "Love-"s;
-  if(b) return result + "Fifteen";
-  return result + "All";
+  if(a) return "Fifteen-Love";
+  if(b) return "Love-Fifteen";
+  return "Love-All";
 }
 
 TEST_CASE("score of 0,0")
@@ -20,4 +20,9 @@ TEST_CASE("score of 0,0")
 TEST_CASE("score of 0,1")
 {
   CHECK(score(0, 1) == "Love-Fifteen");
+}
+
+TEST_CASE("score of 1,0")
+{
+  CHECK(score(1, 0) == "Fifteen-Love");
 }
