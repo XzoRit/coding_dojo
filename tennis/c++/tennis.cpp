59f482b8171446faf1e2224b2ba914a863ed3726
XzoRit <xzorit@hotmail.com>
2016-11-04 15:45:49 +0100

    score of 0 1 equals love-fifteen

--- tennis/c++/tennis.cpp
+++ tennis/c++/tennis.cpp
@@ -3,13 +3,21 @@
 #include <string>
 
 using namespace std;
+using namespace std::string_literals;
 
-string score(int, int)
+string score(int a, int b)
 {
-  return {"Love-All"};
+  auto result = "Love-"s;
+  if(b) return result + "Fifteen";
+  return result + "All";
 }
 
 TEST_CASE("score of 0,0")
 {
   CHECK(score(0, 0) == "Love-All");
+}
+
+TEST_CASE("score of 0,1")
+{
+  CHECK(score(0, 1) == "Love-Fifteen");
 }
