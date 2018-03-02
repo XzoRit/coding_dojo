e4365fe50f80d77ebb424e3a718312cb362b9d3d
XzoRit <xzorit@hotmail.com>
2018-03-02 15:04:16 +0100

    initial state of tennis game is noone has scored yet

--- tennis/c++/tennis_v2.cpp
+++ tennis/c++/tennis_v2.cpp
@@ -1,12 +1,23 @@
 #include <doctest.h>
+#include <string>
 
 namespace
 {
 }
 namespace v2
 {
-TEST_CASE("blah")
+    using namespace std;
+    using namespace std::string_literals;
+
+    struct game
+    {};
+
+    const auto draw = [](const auto&){ return "0-0"s; };
+
+TEST_CASE("game")
 {
-    CHECK(false);
+    game g{};
+    const auto a = draw(g);
+    CHECK(a == "0-0");
 }
 }
