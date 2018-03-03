f2d924d54714e318bc21b2d8b5aad55d92f36f54
XzoRit <xzorit@hotmail.com>
2018-03-03 12:47:50 +0100

    player_1 can win the simple game

--- tennis/c++/tennis_v2.cpp
+++ tennis/c++/tennis_v2.cpp
@@ -1,5 +1,6 @@
 #include <doctest.h>
 #include <iostream>
+#include <optional>
 #include <sstream>
 #include <string>
 
@@ -15,13 +16,15 @@ enum class point
 {
     Love,
     Fifteen,
-    Thirty
+    Thirty,
+    Forty
 };
 
 point operator++(point p)
 {
     if(p == point::Love) return point::Fifteen;
     if(p == point::Fifteen) return point::Thirty;
+    if(p == point::Thirty) return point::Forty;
     return p;
 }
 
@@ -40,6 +43,7 @@ struct game
 {
     player player_1;
     player player_2;
+    optional<player> won_by;
 };
 
 struct player_scored
@@ -54,6 +58,10 @@ const auto inc_score_of = [](const auto& p)
 
 const auto update = [](const auto& g, const auto& act)
 {
+    if(act.scored_player.points == point::Forty)
+    {
+        return game{g.player_1, g.player_2, act.scored_player};
+    }
     if(act.scored_player == g.player_1)
         return game{inc_score_of(g.player_1), g.player_2};
     else return game{g.player_1, inc_score_of(g.player_2)};
@@ -64,6 +72,7 @@ ostream& operator<<(ostream& str, const point& p)
     if(p == point::Love) str << "0";
     else if(p == point::Fifteen) str << "15";
     else if(p == point::Thirty) str << "30";
+    else if(p == point::Forty) str << "40";
     return str;
 }
 
@@ -75,8 +84,15 @@ ostream& operator<<(ostream& str, const player& p)
 
 ostream& operator<<(ostream& str, const game& g)
 {
-    str << g.player_1 << " vs. " << g.player_2;
-    str << '\n';
+    if(g.won_by)
+    {
+        str << (*g.won_by).name << ": won\n";
+    }
+    else
+    {
+        str << g.player_1 << " vs. " << g.player_2;
+        str << '\n';
+    }
     return str;
 }
 
@@ -92,19 +108,38 @@ TEST_CASE("game")
     const player player_1{"player_1", point::Love};
     const player player_2{"player_2", point::Love};
     const game g{player_1, player_2};
+
     const auto a = draw(g);
     REQUIRE(a == "player_1: 0 vs. player_2: 0\n"s);
 
-    SUBCASE("player_1 scores")
+    SUBCASE("player_1 scores once")
     {
-        const auto new_g = update(g, player_scored{player_1});
-        const auto b = draw(new_g);
+        const auto g_1 = update(g, player_scored{player_1});
+        const auto b = draw(g_1);
         REQUIRE(b == "player_1: 15 vs. player_2: 0\n"s);
+        SUBCASE("player_1 twice")
+        {
+            const auto g_2 = update(g_1, player_scored{g_1.player_1});
+            const auto b = draw(g_2);
+            REQUIRE(b == "player_1: 30 vs. player_2: 0\n"s);
+            SUBCASE("player_1 thrice")
+            {
+                const auto g_3 = update(g_2, player_scored{g_2.player_1});
+                const auto b = draw(g_3);
+                REQUIRE(b == "player_1: 40 vs. player_2: 0\n"s);
+                SUBCASE("player_1 won")
+                {
+                    const auto g_4 = update(g_3, player_scored{g_3.player_1});
+                    const auto b = draw(g_4);
+                    REQUIRE(b == "player_1: won\n"s);
+                }
+            }
+        }
     }
     SUBCASE("player_2 scores")
     {
-        const auto new_g = update(g, player_scored{player_2});
-        const auto b = draw(new_g);
+        const auto g_1 = update(g, player_scored{player_2});
+        const auto b = draw(g_1);
         REQUIRE(b == "player_1: 0 vs. player_2: 15\n"s);
     }
 }
