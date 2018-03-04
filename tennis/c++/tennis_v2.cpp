5b6725e8159acb1dfbfab16806c35d3bd6cdc55d
XzoRit <xzorit@hotmail.com>
2018-03-04 13:54:25 +0100

    replace model with separate event types for players scoring

--- tennis/c++/tennis_v2.cpp
+++ tennis/c++/tennis_v2.cpp
@@ -40,17 +40,6 @@ bool operator==(const player& p1, const player& p2)
     return (p1.name == p2.name);
 }
 
-struct model
-{
-    player player_1;
-    player player_2;
-    point points_1;
-    point points_2;
-    bool deuce;
-    optional<player> advantage;
-    optional<player> winning;
-};
-
 struct game
 {
     struct simple
@@ -74,6 +63,14 @@ struct player_scored
 
 using action = player_scored;
 
+struct player_1_scored
+{};
+
+struct player_2_scored
+{};
+
+using score_action = variant<player_1_scored, player_2_scored>;
+
 const auto inc_score_of = [](const auto& p)
 {
     return player{p.name, ++p.points};
@@ -85,8 +82,8 @@ struct do_action
     {
         game new_game{};
         if(act.scored_player == g.player_1
-           &&
-           g.player_1.points == point::Forty)
+                &&
+                g.player_1.points == point::Forty)
         {
             new_game.state =
                 game::winner{act.scored_player};
@@ -108,13 +105,46 @@ struct do_action
         return game{};
     }
     action act;
-    model current;
 };
 
-const auto update = [](const auto& g, const auto& act)
+
+struct update_player_1_scored
+{
+    game operator()(const game::simple& g) const
+    {
+        game new_game{};
+        new_game.state =
+            game::simple{inc_score_of(g.player_1), g.player_2};
+        return new_game;
+    }
+    game operator()(const game::winner&) const
+    {
+        return game{};
+    }
+};
+
+struct apply_score_action
+{
+    game operator()(player_1_scored) const
+    {
+        return visit(update_player_1_scored{}, current.state);
+    }
+    game operator()(player_2_scored) const
+    {
+        return visit([&](const auto&){ return current; }, current.state);
+    }
+    const game& current;
+};
+
+auto update(const game& g, const action& act)
 {
     return visit(do_action{act}, g.state);
-};
+}
+
+auto update(const game& g, const score_action& act)
+{
+    return visit(apply_score_action{g}, act);
+}
 
 ostream& operator<<(ostream& str, const point& p)
 {
@@ -148,9 +178,9 @@ ostream& operator<<(ostream& str, const game::winner& g)
 
 ostream& operator<<(ostream& str, const game& g)
 {
-    visit([&](const auto& a) mutable
-          { str << a; }
-          , g.state);
+    visit([&](const auto & a) mutable
+    { str << a; }
+    , g.state);
     return str;
 }
 
@@ -171,6 +201,12 @@ TEST_CASE("game")
     const auto a = draw(g);
     REQUIRE(a == "player_1: 0 vs. player_2: 0\n"s);
 
+    SUBCASE("player_1 scores once model")
+    {
+        const auto g_1 = update(g, score_action{player_1_scored{}});
+        const auto b = draw(g_1);
+        CHECK(b == "player_1: 15 vs. player_2: 0\n"s);
+    }
     SUBCASE("player_1 scores once")
     {
         const auto g_1 = update(g, player_scored{player_1});
