5349eb2e78290f2ffcb713237ddf63797a6be094
XzoRit <xzorit@hotmail.com>
2019-02-17 17:21:56 +0100

    use free write function instead of forwarding write to a paper owned by pencil

--- pencil/c++/main.cpp
+++ pencil/c++/main.cpp
@@ -41,12 +41,11 @@ namespace xzr::pencil
         };
         struct pen
         {
-            pen(paper* p, durability d)
-                : paper_{p}
+            pen(durability d)
+                : initial_durability_{d}
                 , durability_{d}
-                , initial_durability_{d}
                 {}
-            void write(const std::string& txt)
+            std::string write(const std::string& txt)
                 {
                     std::string s{};
                     for(auto c : txt)
@@ -55,20 +54,19 @@ namespace xzr::pencil
                         else s += ' ';
                         durability_.degrade(c);
                     }
-                    if(paper_) paper_->write(s);
+                    return s;
                 }
             void sharpen()
                 {
                     durability_ = initial_durability_;
                 }
-            void exchange_paper(paper* p)
-                {
-                    paper_ = p;
-                }
-            paper* paper_;
+            const durability initial_durability_;
             durability durability_;
-            durability initial_durability_;
         };
+        void write(const std::string& txt, pen& pen, paper& sheet)
+        {
+            sheet.write(pen.write(txt));
+        }
     }
 }
 namespace
@@ -80,37 +78,36 @@ namespace
     TEST_CASE("writing to causes a pencil point to go dull")
     {
         paper sheet{};
-        pen pencil{&sheet, durability{8}};
+        pen pencil{durability{8}};
 
-        pencil.write("abc");
+        write("abc", pencil, sheet);
         CHECK(sheet.text() == "abc");
         SUBCASE("append")
         {
-            pencil.write("def");
+            write("def", pencil, sheet);
             CHECK(sheet.text() == "abcdef");
             SUBCASE("dull")
             {
                 SUBCASE("lower case")
                 {
-                    pencil.write("ghi");
+                    write("ghi", pencil, sheet);
                     CHECK(sheet.text() == "abcdefgh ");
                     SUBCASE("sharpen")
                     {
-                        paper new_sheet{};
-                        pencil.exchange_paper(&new_sheet);
                         pencil.sharpen();
-                        pencil.write("abcdefghi");
+                        paper new_sheet{};
+                        write("abcdefghi", pencil, new_sheet);
                         CHECK(new_sheet.text() == "abcdefgh ");
                     }
                 }
                 SUBCASE("upper case")
                 {
-                    pencil.write("GHI");
+                    write("GHI", pencil, sheet);
                     CHECK(sheet.text() == "abcdefG  ");
                 }
                 SUBCASE("whitespace")
                 {
-                    pencil.write("   gh");
+                    write("   gh", pencil, sheet);
                     CHECK(sheet.text() == "abcdef   gh");
                 }
             }
