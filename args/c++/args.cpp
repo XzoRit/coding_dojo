66cecfd712195456f371a6520287649636e10f16
XzoRit <xzorit@hotmail.com>
2019-05-12 14:46:44 +0200

    create c++ project structure for args kata

--- args/c++/args.cpp
+++
@@ -1,8 +0,0 @@
-// clang -I../../../c++/catch/single_include -stdlib=libstd -std=c++11 -o args -c args.cpp
-#define CATCH_CONFIG_MAIN
-#include <catch.hpp>
-
-TEST_CASE("", "")
-{
-  REQUIRE(1 == 0);
-}
---
+++ args/c++/args/main.cpp
@@ -0,0 +1,9 @@
+#include <libargs/args.hpp>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello args\n";
+
+    return xzr::args::add(0, 0);
+}
