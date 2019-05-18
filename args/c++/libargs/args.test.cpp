072557f75ea419352c3b00f1007ee682cb05ce4a
XzoRit <xzorit@hotmail.com>
2019-05-18 16:05:29 +0200

    use boost.test in cmake build files

--- args/c++/libargs/args.test.cpp
+++ args/c++/libargs/args.test.cpp
@@ -1,8 +1,9 @@
 #include <libargs/args.hpp>
+#define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 
 BOOST_AUTO_TEST_CASE(libargs_add)
 {
   const auto a{ xzr::args::add(1, 3) };
-  BOOST_TEST(a == 4);
+  BOOST_TEST(a != 4);
 }
--- args/c++/libargs/main_test.cpp
+++
@@ -1,7 +0,0 @@
-#include <libargs/args.hpp>
-
-int main()
-{
-  const auto a{ xzr::args::add(1, 3) };
-  return (a == 4);
-}
